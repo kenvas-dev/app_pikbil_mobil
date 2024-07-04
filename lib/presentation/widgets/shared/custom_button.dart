1b773721ae564c5f6245a619c6f50313af4fc686
kenvas-dev <kevin.aquino.vasquez@gmail.com>
2024-07-03 22:32:01 -0600

    create custom button widget

---
+++ lib/presentation/widgets/shared/custom_button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final VoidCallback voidCallback;
+  final EdgeInsetsDirectional margin;
+  final BorderRadius borderRadius;
+  final Color colorButton;
+  final Text textButton;
+
+  const CustomButton(
+      {super.key,
+      required this.voidCallback,
+      required this.margin,
+      required this.borderRadius,
+      required this.colorButton,
+      required this.textButton});
+
+  @override
+  Widget build(BuildContext context) {
+    final double widthViewport = MediaQuery.of(context).size.width;
+    return GestureDetector(
+      onTap: voidCallback,
+      child: Container(
+        margin: margin,
+        child: ClipRRect(
+          borderRadius: borderRadius,
+          child: Container(
+            width: widthViewport * 0.70,
+            color: colorButton,
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: textButton,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
