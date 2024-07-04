d51a8edc77f9ac30e2291bf50115870a89459fa0
kenvas-dev <kevin.aquino.vasquez@gmail.com>
2024-07-03 20:05:55 -0600

    added export widgets

--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -3,3 +3,4 @@ export './profile/profile_menu_options.dart';
 export './shared/custom_floating_button.dart';
 export './shared/custom_gradient_box.dart';
 export './shared/custom_textfield.dart';
+export 'shared/custom_switch.dart';
