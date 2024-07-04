9a67ba5f9945d44b5376b1654014fe02699be5bc
kenvas-dev <kevin.aquino.vasquez@gmail.com>
2024-07-03 20:06:10 -0600

    format code with prettierc

--- lib/config/static/informative_texts.dart
+++ lib/config/static/informative_texts.dart
@@ -23,14 +23,28 @@ class InformativeTexts {
   static const String billingScreenTargetNumber = '0000000012345678';
   static const String billingScreenTargetClient = 'Kevin Aquino';
   static const String billingScreenTargetExpire = '09/24';
-  static const String billingScreenAddMorePaymentMethod = 'Add more payment method';
-  
+  static const String billingScreenAddMorePaymentMethod =
+      'Add more payment method';
+
   static const String changePasswordScreenChangePassword = 'Change Password';
   static const String changePasswordScreenOldPassword = 'Old Password';
-  static const String changePasswordScreenOldPasswordPlaceholder = 'Old Password';
+  static const String changePasswordScreenOldPasswordPlaceholder =
+      'Old Password';
   static const String changePasswordScreenNewPassword = 'New Password';
-  static const String changePasswordScreenNewPasswordPlaceholder = 'New Password';
-  static const String changePasswordScreenNewPasswordRetype = 'Retype new Password';
-  static const String changePasswordScreenNewPasswordRetypePlaceholder = 'Retype new Password';
+  static const String changePasswordScreenNewPasswordPlaceholder =
+      'New Password';
+  static const String changePasswordScreenNewPasswordRetype =
+      'Retype new Password';
+  static const String changePasswordScreenNewPasswordRetypePlaceholder =
+      'Retype new Password';
   static const String changePasswordScreenNewPasswordButton = 'Change Password';
+
+  static const String notificationScreenNotification = 'Notification';
+  static const String notificationScreenReceiveMessage =
+      'Receive message from pikibil';
+  static const String notificationScreenReceiveCoupons =
+      'Receive coupons & promotion';
+  static const String notificationScreenReceiveNewsletter =
+      'Receive newsletter from pikibil';
+  static const String notificationScreenUpdate = 'Update about pikibil apps';
 }
