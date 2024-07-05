8771ca4a35d8fd5c3c4d8701c1a29ad93c4a2a58
kenvas-dev <kevin.aquino.vasquez@gmail.com>
2024-07-05 14:33:58 -0600

    added new image

--- lib/config/static/path_image.dart
+++ lib/config/static/path_image.dart
@@ -5,6 +5,13 @@ class PathImage {
   static const String changePassword = "$assetsImagesDir/password.png";
   static const String notification = "$assetsImagesDir/notification.png";
   static const String visa = "$assetsImagesDir/visa.png";
+  static const String key = "$assetsImagesDir/key.png";
+  static const String flag = "$assetsImagesDir/flag.png";
+  static const String loked = "$assetsImagesDir/loked.png";
+  static const String ok = "$assetsImagesDir/ok.png";
+  static const String yeah = "$assetsImagesDir/yeah.png";
+  static const String facebook = "$assetsImagesDir/facebook.png";
+  static const String gmail = "$assetsImagesDir/gmail.png";
   static const String welcome1 = "$assetsImagesDir/cars/welcome1.jpg";
   static const String welcome2 = "$assetsImagesDir/cars/welcome2.jpg";
   static const String welcome3 = "$assetsImagesDir/cars/welcome3.jpg";
