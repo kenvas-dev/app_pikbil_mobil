812bc34ac91c74611116485553b088a64bcba06a
kenvas-dev <kevin.aquino.vasquez@gmail.com>
2024-07-05 14:34:27 -0600

    added new texts

--- lib/config/static/custom_text.dart
+++ lib/config/static/custom_text.dart
@@ -11,42 +11,41 @@ class CustomText {
       fontFamily: 'Gotham-Medium', fontSize: 36, color: CustomColor.primary000);
   static const TextStyle headlineDark800 = TextStyle(
       fontFamily: 'Gotham-Medium', fontSize: 36, color: CustomColor.primary900);
-  
+
   static const TextStyle headlineWhite700 = TextStyle(
       fontFamily: 'Gotham-Medium', fontSize: 24, color: CustomColor.primary000);
   static const TextStyle headlineDark700 = TextStyle(
       fontFamily: 'Gotham-Medium', fontSize: 24, color: CustomColor.primary900);
-  
+
   static const TextStyle headlineWhite600 = TextStyle(
       fontFamily: 'Gotham-Medium', fontSize: 20, color: CustomColor.primary000);
   static const TextStyle headlineDark600 = TextStyle(
       fontFamily: 'Gotham-Medium', fontSize: 20, color: CustomColor.primary900);
-  
+
   static const TextStyle headlineWhite500 = TextStyle(
       fontFamily: 'Gotham-Medium', fontSize: 16, color: CustomColor.primary000);
   static const TextStyle headlineDark500 = TextStyle(
       fontFamily: 'Gotham-Medium', fontSize: 16, color: CustomColor.primary900);
-  
+
   static const TextStyle headlineWhite400 = TextStyle(
       fontFamily: 'Gotham-Medium', fontSize: 14, color: CustomColor.primary000);
   static const TextStyle headlineDark400 = TextStyle(
       fontFamily: 'Gotham-Medium', fontSize: 14, color: CustomColor.primary900);
-  
+
   static const TextStyle headlineWhite300 = TextStyle(
       fontFamily: 'Gotham-Medium', fontSize: 12, color: CustomColor.primary000);
   static const TextStyle headlineDark300 = TextStyle(
       fontFamily: 'Gotham-Medium', fontSize: 12, color: CustomColor.primary900);
-  
+
   static const TextStyle headlineWhite200 = TextStyle(
       fontFamily: 'Gotham-Medium', fontSize: 12, color: CustomColor.primary000);
   static const TextStyle headlineDark200 = TextStyle(
       fontFamily: 'Gotham-Medium', fontSize: 12, color: CustomColor.primary900);
-  
+
   static const TextStyle headlineWhite100 = TextStyle(
       fontFamily: 'Gotham-Medium', fontSize: 10, color: CustomColor.primary000);
   static const TextStyle headlineDark100 = TextStyle(
       fontFamily: 'Gotham-Medium', fontSize: 10, color: CustomColor.primary900);
-
 
   static const TextStyle headlineBookWhite900 = TextStyle(
       fontFamily: 'Gotham-Book', fontSize: 48, color: CustomColor.primary000);
@@ -57,39 +56,53 @@ class CustomText {
       fontFamily: 'Gotham-Book', fontSize: 36, color: CustomColor.primary000);
   static const TextStyle headlineBookDark800 = TextStyle(
       fontFamily: 'Gotham-Book', fontSize: 36, color: CustomColor.primary900);
-  
+
   static const TextStyle headlineBookWhite700 = TextStyle(
       fontFamily: 'Gotham-Book', fontSize: 24, color: CustomColor.primary000);
   static const TextStyle headlineBookDark700 = TextStyle(
       fontFamily: 'Gotham-Book', fontSize: 24, color: CustomColor.primary900);
-  
+
   static const TextStyle headlineBookWhite600 = TextStyle(
       fontFamily: 'Gotham-Book', fontSize: 20, color: CustomColor.primary000);
   static const TextStyle headlineBookDark600 = TextStyle(
       fontFamily: 'Gotham-Book', fontSize: 20, color: CustomColor.primary900);
-  
+
   static const TextStyle headlineBookWhite500 = TextStyle(
       fontFamily: 'Gotham-Book', fontSize: 16, color: CustomColor.primary000);
   static const TextStyle headlineBookDark500 = TextStyle(
       fontFamily: 'Gotham-Book', fontSize: 16, color: CustomColor.primary900);
-  
+
   static const TextStyle headlineBookWhite400 = TextStyle(
       fontFamily: 'Gotham-Book', fontSize: 14, color: CustomColor.primary000);
   static const TextStyle headlineBookDark400 = TextStyle(
       fontFamily: 'Gotham-Book', fontSize: 14, color: CustomColor.primary900);
-  
+
   static const TextStyle headlineBookWhite300 = TextStyle(
       fontFamily: 'Gotham-Book', fontSize: 12, color: CustomColor.primary000);
   static const TextStyle headlineBookDark300 = TextStyle(
       fontFamily: 'Gotham-Book', fontSize: 12, color: CustomColor.primary900);
-  
+
   static const TextStyle headlineBookWhite200 = TextStyle(
       fontFamily: 'Gotham-Book', fontSize: 12, color: CustomColor.primary000);
   static const TextStyle headlineBookDark200 = TextStyle(
       fontFamily: 'Gotham-Book', fontSize: 12, color: CustomColor.primary900);
-  
+
   static const TextStyle headlineBookWhite100 = TextStyle(
       fontFamily: 'Gotham-Book', fontSize: 10, color: CustomColor.primary000);
   static const TextStyle headlineBookDark100 = TextStyle(
       fontFamily: 'Gotham-Book', fontSize: 10, color: CustomColor.primary900);
+
+  static TextStyle headlineBook(Color color, double fontSize) {
+    return TextStyle(
+        fontFamily: 'Gotham-Book', fontSize: fontSize, color: color);
+  }
+
+  static TextStyle headlineMedium(Color color, double fontSize) {
+    return TextStyle(
+        fontFamily: 'Gotham-Medium', fontSize: fontSize, color: color);
+  }
+
+  static TextStyle headline(Color color, double fontSize) {
+    return TextStyle(fontFamily: 'Gotham', fontSize: fontSize, color: color);
+  }
 }
