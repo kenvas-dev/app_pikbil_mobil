e764c5ef6d5779a717bee2dfdc539cbbe4f3ca50
kenvas-dev <kevin.aquino.vasquez@gmail.com>
2024-07-04 23:38:59 -0600

    default route to welcome screen

--- lib/config/router/path_router.dart
+++ lib/config/router/path_router.dart
@@ -1,6 +1,6 @@
 class PathRouter {
   static const String home = '/';
-  static const String initialLocation = '/profile';
+  static const String initialLocation = '/welcome';
   static const String welcome = '/welcome';
   static const String profile = '/profile';
   static const String account = '/account';
