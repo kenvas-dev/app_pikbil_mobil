eb2ed560ff45e82b7ed66924034570109b555e19
kenvas-dev <kevin.aquino.vasquez@gmail.com>
2024-07-05 14:37:05 -0600

    create new password screen

---
+++ lib/presentation/screens/credentials/create_new_password.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CreateNewPasswordScreen extends StatelessWidget {
+  static const String name = 'create-new-password-screen';
+  const CreateNewPasswordScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
