3488542a1924c1ab312e351f663ee0e4caa52d77
kenvas-dev <kevin.aquino.vasquez@gmail.com>
2024-07-05 14:34:38 -0600

    added new text

--- lib/config/static/informative_texts.dart
+++ lib/config/static/informative_texts.dart
@@ -51,6 +51,9 @@ class InformativeTexts {
       'Receive newsletter from pikibil';
   static const String notificationScreenUpdate = 'Update about pikibil apps';
 
+  static const String welcomeScreenSkipBtn = 'Skip';
+  static const String welcomeScreenLoginBtn = 'Login';
+  static const String welcomeScreenNextBtn = 'Next';
   static const String welcomeScreenEndlessOption = 'Endless option';
   static const String welcomeScreenEndlessOptionDescriptions =
       'Quis exercitation ullamco aliquip pariatur amet enim anim cillum adipisicing.';
@@ -60,4 +63,56 @@ class InformativeTexts {
   static const String welcomeScreenDriveSupport = '24/7 Support';
   static const String welcomeScreenDriveSupportDescriptions =
       'Quis exercitation ullamco aliquip pariatur amet enim anim cillum adipisicing.';
+
+  static const String loginScreenWelcome = 'Welcome to, Pikbil';
+  static const String loginScreenWelcomeDescription =
+      'Officia aliquip deserunt Lorem ut aliqua magna nulla minim.';
+  static const String loginScreenEmail = 'Email address';
+  static const String loginScreenEmailPlaceholder = 'Your email address';
+  static const String loginScreenPassword = 'Password';
+  static const String loginScreenPasswordPlaceholder = 'Your password';
+  static const String loginScreenLoginBtn = 'Login';
+  static const String loginScreenForgotPassword = 'Forgot password?';
+  static const String loginScreenLoginWith = 'or login with';
+  static const String loginScreenQuestion = 'Ex eu duis elit officia?';
+  static const String loginScreenRegister = 'Register';
+
+  static const String registerScreenWelcome = 'Nice to know you!';
+  static const String registerScreenWelcomeDescription =
+      'Exercitation quis eu est labore veniam aliqua elit.';
+  static const String registerScreenFullName = 'Full name';
+  static const String registerScreenFullNamePlaceholder = 'Your full name';
+  static const String registerScreenEmail = 'Email address';
+  static const String registerScreenEmailPlaceholder = 'Your email address';
+  static const String registerScreenPassword = 'Password';
+  static const String registerScreenPasswordPlaceholder = 'Your password';
+  static const String registerScreenRegisterBtn = 'Register';
+  static const String registerScreenRegisterWith = 'or register with';
+  static const String registerScreenQuestion = 'Pariatur sunt proident?';
+  static const String registerScreenLogin = 'Login';
+
+  static const String checkEmailScreenWelcome = 'Check your email';
+  static const String checkEmailScreenWelcomeDescription =
+      'Voluptate pariatur sit commodo irure sit est sint do ipsum esse et elit laboris.';
+  static const String checkEmailScreenContinueBtn = 'Continue';
+  static const String checkEmailScreenQuestion =
+      'Deserunt ipsum tempor?';
+  static const String checkEmailScreenResend = 'Resend';
+
+  static const String forgotPassScreenWelcome = 'Forgot Password?';
+  static const String forgotPassScreenWelcomeDescription =
+      'Labore reprehenderit sunt cillum dolor magna quis commodo Lorem in sit culpa officia dolor.';
+  static const String forgotPassScreenEmail = 'Email address';
+  static const String forgotPassScreenEmailPlaceholder = 'Your email address';
+  static const String forgotPassScreenRecoverAccount = 'Recover account';
+
+  static const String newPassScreenWelcome = 'Create new password?';
+  static const String newPassScreenWelcomeDescription =
+      'Magna exercitation enim reprehenderit proident.';
+  static const String newPasScreenPassword = 'Password';
+  static const String newPassScreenPasswordPlaceholder = 'Your password';
+  static const String newPasScreenRetypePassword = 'Retype password';
+  static const String newPasScreenRetypePasswordPlaceholder =
+      'Retype your password';
+  static const String newPasScreenConfirmPasswordBtn = 'Confirm your password';
 }
